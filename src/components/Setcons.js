2e25a0ade08a9bc0dc82a04759673ca43b54fe21
coin0527 <nako2020@naver.com>
2023-12-05 16:09:21 +0900

    .

--- src/components/Setcons.js
+++ src/components/Setcons.js
@@ -4,4 +4,4 @@ export const LOGIN_URL =
 export const IMG_404 =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3Q3yQpFUaJjvbHVXczJokAiIOxZHmbVxz0g&usqp=CAU";
 export const IMG_NOT =
-  "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA3MjJfOTQg%2FMDAxNjI2OTE3NjgwMTIy.aJigD_IlcPD1YOyeekh-whLtNSMii4Ub6VI1ErXW2r4g.7Nga4sgVTnfMujF6sEGxaYGwcGSvWvgAX5IIhlAEpjAg.PNG.dct8867%2F%25B9%25B0%25C0%25BD%25C7%25A5_%25C8%25AD%25C0%25CC%25C6%25AE_%25C0%25CF%25B7%25AF%25BD%25BA%25C6%25AE4x.png&type=a340";
+  "https://search.pstatic.net/sunny/?src=http%3A%2F%2Fmedia.istockphoto.com%2Fvectors%2Fquestion-mark-icon-iconic-series-vector-id540567190%3Fk%3D6%26m%3D540567190%26s%3D170667a%26w%3D0%26h%3Dxe-AzPkYqr1SNtZtL7nLTZ-YfWZphYyhnyU1tJ1mClY%3D&type=a340";
