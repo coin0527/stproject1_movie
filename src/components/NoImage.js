a84f803739f653b727155c1f1cc6a099d29f08ee
coin0527 <nako2020@naver.com>
2023-12-05 16:05:12 +0900

    .

--- src/components/NoImage.js
+++ src/components/NoImage.js
@@ -3,9 +3,12 @@ import { IMG_NOT } from "./Setcons";
 
 const Wrap = styled.div`
   background: url(${IMG_NOT}) no-repeat center / cover;
-  width: 500px;
-  max-width: 300px;
+  width: 100%;
+  max-width: 100%;
   height: 300px;
+  @media screen and (max-width: 500px) {
+    height: 350px;
+  }
 `;
 
 export const NoImage = () => {
